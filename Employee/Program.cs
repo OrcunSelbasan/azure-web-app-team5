126fe8a8864aa843105b998605f8f7c0e3614797
Orcun Selbasan <lutfuorcunselbasan@gmail.com>
2022-12-30 23:01:17 +0300

    Comment out the environment condition

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -17,16 +17,18 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (app.Environment.IsDevelopment())
+//{
+    
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+app.UseMigrationsEndPoint();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
